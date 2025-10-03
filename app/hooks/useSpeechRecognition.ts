c143e499294319126cdee7be2bdd798891e22ff8
mathieu-mychatbotai <mathieu@mychatbotai.fr>
2025-10-03 14:57:26 +0200

    Updated app.json

--- app/hooks/useSpeechRecognition.ts
+++
@@ -1,7 +0,0 @@
-await ExpoSpeechRecognitionModule.start({
-  lang: "fr-FR", // ← Assurez-vous que c'est bien "fr-FR" et pas "en-US"
-  interimResults: true,
-  maxAlternatives: 1,
-  continuous: false,
-  requiresOnDeviceRecognition: false,
-});
